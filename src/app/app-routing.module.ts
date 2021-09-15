c23fed8d60f420d85f2f305fa86c8025719e7bff
Marco Lauinger <marco@thingsthinking.net>
2021-09-15 10:08:48 +0200

    setup routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailedForecastComponent } from './detailed-forecast/detailed-forecast.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', component: HomeComponent}, {path: 'forecast/:zipcode', component: DetailedForecastComponent}];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
